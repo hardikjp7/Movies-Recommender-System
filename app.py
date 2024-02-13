6e33179de4d2e6ac650e50e1c84021a5b5a5fc8a
Hardik Parmar <121241956+hardikjp7@users.noreply.github.com>
2024-02-13 11:04:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-'''
-Author: Hardik Parmar
-Email: HardikJParmar7@gmail.com
-'''
-
 import pickle
 import streamlit as st
 import requests
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=ace05b317404492bc2d3cb7abe311664&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    full_path = "https://image.tmdb.org/t/p/original" + poster_path
     return full_path
 
 def recommend(movie):
